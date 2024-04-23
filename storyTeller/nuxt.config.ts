4c2ded087bc04d1a4bb7d2cce17ed42e8590185f
hhhhaaarry <94174124+hhhhaaarry@users.noreply.github.com>
2024-04-23 18:21:32 +0200

    Added Tailwind Css

--- storyTeller/nuxt.config.ts
+++ storyTeller/nuxt.config.ts
@@ -1,4 +1,10 @@
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    css: [  "~/assets/styles/main.css"],
+    modules: ["@nuxtjs/tailwindcss"],
+    tailwindcss: {
+        cssPath: ["~/assets/styles/main.css", { injectPosition: "first" }],
+        configPath: "tailwind.config",
+        editorSupport: true,
+      },
 })
--- storyTeller/tailwind.config.js
+++ storyTeller/tailwind.config.js
@@ -1,7 +1,24 @@
 export default {
-  content: [],
+  content: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./composables/**/*.{js,ts}",
+    "./plugins/**/*.{js,ts}",
+    "./utils/**/*.{js,ts}",
+    "./App.{js,ts,vue}",
+    "./app.{js,ts,vue}",
+    "./Error.{js,ts,vue}",
+    "./error.{js,ts,vue}",
+    "./app.config.{js,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'press-start': ['Press Start 2P', 'cursive'],
+        arial: ['Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
   
