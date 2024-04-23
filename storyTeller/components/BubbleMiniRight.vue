1cf561d907607043adbd24af4d271ec97d5917a3
hhhhaaarry <94174124+hhhhaaarry@users.noreply.github.com>
2024-04-23 18:22:43 +0200

    Fixed Styles and Props of BubbleMiniRight

--- storyTeller/components/BubbleMiniRight.vue
+++ storyTeller/components/BubbleMiniRight.vue
@@ -1,5 +1,18 @@
 <template>
-    <div style="position: relative; margin: 20px; text-align: center; font-family: 'Press Start 2P', cursive; font-size: 16px; background-color: white; color: black; padding: 2px; box-shadow: 0 -4px white, 0 -8px black, 4px 0 white, 4px -4px black, 8px 0 black, 0 4px white, 0 8px black, -4px 0 white, -4px 4px black, -8px 0 black, -4px -4px black, 4px 4px black; width: 50px;">
-        ...
+    <div class="bubble-mini-right" :style="`left: ${props.x}px; top: ${props.y}px;`">
+        {{message}}
     </div>
 </template>
+<script setup lang="ts">
+const props = defineProps<{
+    message: string | '...',
+    x: number,
+    y: number,
+}>()
+</script>
+<style lang="postcss" scoped>
+.bubble-mini-right{
+    @apply absolute z-40 m-5 text-center font-press-start italic text-base bg-white text-black p-[2px] px-2 w-fit transform -translate-x-1/2 -translate-y-1/2;
+    box-shadow: 0 -4px white, 0 -8px black, 4px 0 white, 4px -4px black, 8px 0 black, 0 4px white, 0 8px black, -4px 0 white, -4px 4px black, -8px 0 black, -4px -4px black, 4px 4px black;
+}
+</style>
