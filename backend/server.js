1c8d3c779cdf428140eec75ea98ec64c8f809835
Evelyn <lhnguye1@ualberta.ca>
2024-04-24 10:57:24 -0600

    feat: Initialize Node.js backend with Express

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(3000, () => console.log('Server running on port 3000'))
