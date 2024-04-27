15edb225561630467b454902c5fb074679d85c88
hhhhaaarry <94174124+hhhhaaarry@users.noreply.github.com>
2024-04-27 17:19:58 +0200

    Added hover to login nav

--- frontend/components/LoginNav.vue
+++ frontend/components/LoginNav.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="login-nav flex h-4 justify-center items-center gap-6">
-        <NuxtLink to="/login" class="text-slate-400 text-base font-normal leading-5">Login</NuxtLink>
-        <NuxtLink to="/register" class="text-slate-400 text-base font-normal leading-5">Register</NuxtLink>
+        <NuxtLink to="/login" class="text-slate-400 hover:text-slate-500 transition-colors duration-150 text-base font-normal leading-5">Login</NuxtLink>
+        <NuxtLink to="/register" class="text-slate-400 hover:text-slate-500 transition-colors duration-150 text-base font-normal leading-5">Register</NuxtLink>
     </nav>
 </template>
 
