d95f72415d5d68f3a8dbe3185573196bec254f35
hhhhaaarry <94174124+hhhhaaarry@users.noreply.github.com>
2024-04-27 14:10:14 +0200

    Added Layout and Hero

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    id="app"
+    class="layout layout--default h-screen w-screen bg-gradient-to-br from-[#1E293B] to-[#020617] relative"
+  >
+    <TopBar />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="postcss">
+body,
+html {
+  @apply font-work;
+}
+</style>
