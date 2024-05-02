50946aa48e4c7fe2a860f621ca7f7b541c5da348
hhhhaaarry <94174124+hhhhaaarry@users.noreply.github.com>
2024-05-02 10:49:00 +0200

    Removed draft from store

--- frontend/stores/storyStore.ts
+++ frontend/stores/storyStore.ts
@@ -2,54 +2,49 @@ import { defineStore } from "pinia";
 
 export const useStoryStore = defineStore("story", {
   state: () => ({
-    persona: "You are an award-winning science fiction author with a penchant for expansive, intricately woven stories. Your ultimate goal is to write the next award winning adventure story." as string,
+    persona:
+      "You are an award-winning science fiction author with a penchant for expansive, intricately woven stories. Your ultimate goal is to write the next award winning adventure story." as string,
     premise: null as string | null,
     outline: null as string | null,
-    draft: null as string | null,
     finalStory: null as string | null,
   }),
   actions: {
     setPersona(persona: string) {
-        this.persona = persona;
-      },
+      this.persona = persona;
+    },
     setPremise(premise: string) {
       this.premise = premise;
     },
     setOutline(outline: string) {
       this.outline = outline;
     },
-    setDraft(draft: string) {
-      this.draft = draft;
-    },
+
     setFinalStory(finalStory: string) {
       this.finalStory = finalStory;
     },
     resetStory() {
-        this.persona = "You are an award-winning science fiction author with a penchant for expansive, intricately woven stories. Your ultimate goal is to write the next award winning adventure story.";
-        this.premise = null;
-        this.outline = null;
-        this.draft = null;
-        this.finalStory = null;
-      }
+      this.persona =
+        "You are an award-winning science fiction author with a penchant for expansive, intricately woven stories. Your ultimate goal is to write the next award winning adventure story.";
+      this.premise = null;
+      this.outline = null;
+      this.finalStory = null;
+    },
   },
   getters: {
     getPersona: (state) => state.persona,
     getPremise: (state) => state.premise,
     getOutline: (state) => state.outline,
-    getDraft: (state) => state.draft,
     getFinalStory: (state) => state.finalStory,
     hasPremise: (state) => state.premise !== null,
     hasOutline: (state) => state.outline !== null,
-    hasDraft: (state) => state.draft !== null,
     hasFinalStory: (state) => state.finalStory !== null,
     isStoryComplete: (state) =>
       state.finalStory !== null && state.finalStory.includes("IAMDONE"),
-      fullStoryState: (state) => ({
-        persona: state.persona,
-        premise: state.premise,
-        outline: state.outline,
-        draft: state.draft,
-        finalStory: state.finalStory
-      })
+    fullStoryState: (state) => ({
+      persona: state.persona,
+      premise: state.premise,
+      outline: state.outline,
+      finalStory: state.finalStory,
+    }),
   },
 });
